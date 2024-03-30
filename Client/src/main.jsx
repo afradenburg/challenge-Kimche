6e2b6372b382cbf16f1939ca5513b2258203afe0
afradenburg <andres.burg@hotmail.com>
2024-03-30 11:03:37 -0500

    config graphql and query with bug

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
+import { ApolloProvider} from "react-apollo"
+import ApolloClient,{InMemoryCache} from "apollo-boost"
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+   cache: new InMemoryCache(),
+})
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+      <BrowserRouter>
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+      </BrowserRouter>
   </React.StrictMode>,
 )
--- Client/src/views/home.jsx
+++ Client/src/views/home.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { gql } from "apollo-boost";
+
+const CharactersQuery = () => {
+  return (
+    <Query
+      query={gql`
+        {
+          characters {
+            results {
+              id
+              name
+              species
+            }
+          }
+        }
+      `}
+      fetchPolicy="network-only"
+    >
+      {({ loading, error, data }) => {
+        if (error) return <p>{error.message}</p>;
+        if (loading) return <p>Loading...</p>;
+        return data.characters.results.map((character) => {
+          return (
+          <>
+          <p key={character.id}> {character.name}</p>          
+          </>
+          )
+        });
+      }}
+    </Query>
+  );
+};
 
 export const Home = () => {
   return (
     <div>
       <h1>Home rick como vas</h1>
+      <CharactersQuery />
     </div>
   );
 };
