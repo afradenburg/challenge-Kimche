96650bb466b5ed59f0a3d3c78c9de23c81d0ffeb
afradenburg <andres.burg@hotmail.com>
2024-04-03 15:27:29 -0500

    condicional characters, render Subtitle, add useEffect

--- Client/src/components/Characters.jsx
+++ Client/src/components/Characters.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { CardStyled } from "../styledComponents/cardsStyled";
 import { Character } from "./Character";
 import { Subtitle } from "../styledComponents/filtersStyled";
 
 export const Characters = ({ characters, setSelectCharacter }) => {
+  const [updateCount, setUpdateCount] = useState(0)
+  const [subtitleText, setSubtitleText] = useState("Buscando personajes");
+
+  useEffect(() => {
+    if (characters.length === 0 && updateCount < 1) {
+      setSubtitleText("Buscando Personajes");
+      setUpdateCount((prevCount) => prevCount + 1)
+    } else {
+      setSubtitleText("No Hay Personajes Con Estas Caracteristicas");
+    }
+  }, [characters]);
+
   return (
     <CardStyled className="characters">
       {characters.length === 0 ? (
-        <Subtitle style={{ alignItems: "center" }}>
-          Buscando Personajes
-        </Subtitle>
+        <Subtitle style={{ alignItems: "center" }}>{subtitleText}</Subtitle>
       ) : (
         characters.map((character) => (
           <Character
